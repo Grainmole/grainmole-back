1c6eb73efc0e87a85821d327156b2b7e05eab516
KuhaarTar <flempiefilms@gmail.com>
2023-10-28 13:00:04 +0300

    Initial commit

---
+++ src/main/java/ua/grainmole/GrainMoleBackApplication.java
@@ -0,0 +1,13 @@
+package ua.grainmole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainMoleBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrainMoleBackApplication.class, args);
+    }
+
+}
