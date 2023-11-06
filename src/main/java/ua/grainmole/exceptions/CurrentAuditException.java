a385a1e0c1fd6ef246ec80eee77098f178d3eaeb
KuhaarTar <flempiefilms@gmail.com>
2023-11-06 22:51:54 +0200

    clean up code

--- src/main/java/ua/grainmole/exceptions/CurrentAuditException.java
+++ src/main/java/ua/grainmole/exceptions/CurrentAuditException.java
@@ -1,8 +1,10 @@
 package ua.grainmole.exceptions;
 
 import lombok.experimental.StandardException;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @StandardException
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class CurrentAuditException extends RuntimeException {
 }
--- src/main/java/ua/grainmole/exceptions/NotHaveSuchRightsException.java
+++ src/main/java/ua/grainmole/exceptions/NotHaveSuchRightsException.java
@@ -1,7 +1,10 @@
 package ua.grainmole.exceptions;
 
 import lombok.experimental.StandardException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @StandardException
+@ResponseStatus(code = HttpStatus.FORBIDDEN)
 public class NotHaveSuchRightsException extends RuntimeException{
 }
